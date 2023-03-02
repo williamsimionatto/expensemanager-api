1937f9b19b6f51f314e99dfe460feba66d37a434
William Simionatto <william.sn0226@gmail.com>
2023-03-02 19:57:56 -0300

    feat: implements LoadExpenseCategories in ExpenseCategoryService

--- src/main/java/william/expensemanagerapi/service/ExpenseCategoryService.java
+++ src/main/java/william/expensemanagerapi/service/ExpenseCategoryService.java
@@ -1,15 +1,20 @@
 package william.expensemanagerapi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import william.expensemanagerapi.domain.entities.ExpenseCategory;
 import william.expensemanagerapi.domain.model.AddExpenseCategoryModel;
 import william.expensemanagerapi.domain.usecases.AddExpenseCategory;
+import william.expensemanagerapi.domain.usecases.LoadExpenseCategories;
 import william.expensemanagerapi.repository.ExpenseCategoryRepository;
 
 @Service
-public class ExpenseCategoryService implements AddExpenseCategory {
+public class ExpenseCategoryService implements 
+  AddExpenseCategory,
+  LoadExpenseCategories {
   @Autowired
   private ExpenseCategoryRepository expenseCategoryRepository;
 
@@ -17,5 +22,10 @@ public class ExpenseCategoryService implements AddExpenseCategory {
   public ExpenseCategory add(AddExpenseCategoryModel params) {
     ExpenseCategory expenseCategory = new ExpenseCategory(params);
     return expenseCategoryRepository.save(expenseCategory);
+  }
+
+  @Override
+  public List<ExpenseCategory> load() {
+    return expenseCategoryRepository.findAll();
   }
 }
