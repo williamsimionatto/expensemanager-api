6834ba23a43ba18178fd2857e515b697c7f2712f
William Simionatto <william.sn0226@gmail.com>
2023-03-02 20:28:06 -0300

    feat: create endpoint to edit a ExpenseCategory

--- src/main/java/william/expensemanagerapi/controller/ExpenseCategoryController.java
+++ src/main/java/william/expensemanagerapi/controller/ExpenseCategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import william.expensemanagerapi.domain.entities.ExpenseCategory;
 import william.expensemanagerapi.domain.model.AddExpenseCategoryModel;
+import william.expensemanagerapi.domain.model.EditExpenseCategoryModel;
 import william.expensemanagerapi.service.ExpenseCategoryService;
 
 @RestController
@@ -51,6 +53,15 @@ public class ExpenseCategoryController {
   @GetMapping("/{id}")
   public ResponseEntity<ExpenseCategory> get(@PathVariable Long id) {
     ExpenseCategory expenseCategory = expenseCategoryService.get(id);
+    return ResponseEntity.ok(expenseCategory);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<ExpenseCategory> edit(
+    @PathVariable Long id,
+    @RequestBody @Valid EditExpenseCategoryModel params
+  ) {
+    ExpenseCategory expenseCategory = expenseCategoryService.edit(id, params);
     return ResponseEntity.ok(expenseCategory);
   }
 }
