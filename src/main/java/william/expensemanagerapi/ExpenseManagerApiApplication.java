760017100a77a43cdddeee5c9d13f4d40a2450c2
William Simionatto <william.sn0226@gmail.com>
2023-03-03 22:14:18 -0300

    chore: configure Application Timezone

--- src/main/java/william/expensemanagerapi/ExpenseManagerApiApplication.java
+++ src/main/java/william/expensemanagerapi/ExpenseManagerApiApplication.java
@@ -1,9 +1,13 @@
 package william.expensemanagerapi;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +16,11 @@ public class ExpenseManagerApiApplication {
 		SpringApplication.run(ExpenseManagerApiApplication.class, args);
 		System.out.println("Application Running");
 	}
+
+	@PostConstruct
+  public void init(){
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
 
 	@GetMapping("/hello")
 	public String hello() {
