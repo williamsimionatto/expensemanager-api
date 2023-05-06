e06d4ae51ba54cfa3b84fe670534ae30bb20f8f2
William Simionatto <william.sn0226@gmail.com>
2023-05-06 11:26:03 -0300

    fix: verify if date is in period correctly

--- src/main/java/william/expensemanagerapi/validation/DateValidation.java
+++ src/main/java/william/expensemanagerapi/validation/DateValidation.java
@@ -7,13 +7,12 @@ import java.util.TimeZone;
 public class DateValidation {
   public static boolean isDateInPeriod(Date date, Date startDate, Date endDate) {
     date = new Date(date.getTime() + TimeZone.getDefault().getRawOffset());
-    startDate = addDays(startDate, 1);
-    endDate = addDays(endDate, 1);
-    return date.after(startDate) && date.before(endDate);
+    boolean isInPeriod = (date.equals(startDate) || date.after(startDate)) && (date.equals(endDate) || date.before(endDate));
+
+    return isInPeriod;
   }
 
   public static String formatDate(Date date, String pattern) {
-    date = addDays(date, 1);
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
     return simpleDateFormat.format(date);
   }
